8b74e15ce26bd3c22eebf4cfae6cbe3ae84db497
Jetsom <18514665919@163.com>
2023-02-27 12:37:22 +0800

    简化代码

--- src/wechaty/index.js
+++ src/wechaty/index.js
@@ -1,6 +1,10 @@
-import { WechatyBuilder, ScanStatus, log } from 'wechaty'
+import { WechatyBuilder, ScanStatus } from 'wechaty'
 import qrTerminal from 'qrcode-terminal'
-import { defaultMessage, shardingMessage } from './sendMessage.js'
+import { getOpenAiReply as getReply } from '../openai/index.js'
+import { botName } from '../../config.js'
+
+let bot = {};
+initProject()
 // 扫码
 function onScan(qrcode, status) {
   if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
@@ -9,7 +13,7 @@ function onScan(qrcode, status) {
     const qrcodeImageUrl = ['https://api.qrserver.com/v1/create-qr-code/?data=', encodeURIComponent(qrcode)].join('')
     console.log('onScan:', qrcodeImageUrl, ScanStatus[status], status)
   } else {
-    log.info('onScan: %s(%s)', ScanStatus[status], status)
+    console.log('onScan: %s(%s)', ScanStatus[status], status)
   }
 }
 
@@ -42,36 +46,96 @@ async function onFriendShip(friendship) {
  * @param isSharding
  * @returns {Promise<void>}
  */
+const pre = `[旺柴][chatgpt response][旺柴] \n`
+const chatMap = new Map()
+const startTime = new Date().getTime();
+let contactSelf = {}
 async function onMessage(msg) {
-  // 默认消息回复
-  await defaultMessage(msg, bot)
-  // 消息分片
-  // await shardingMessage(msg,bot)
+  // 避免重复发送
+  let msgTime = msg.date().getTime()
+  if (msgTime < startTime) {
+    return;
+  }
+
+  const contact = msg.talker() // 发消息人
+  if (msg.self()) {
+    contactSelf = contact
+  }
+
+  const receiver = msg.to() // 消息接收人
+  const content = msg.text() // 消息内容
+  const room = msg.room() // 是否是群消息
+  const roomName = (await room?.topic()) || null // 群名称
+  const aliasName = await contact.alias() // 备注名称
+  const name = await contact.name() // 微信名称
+  const isText = msg.type() === bot.Message.Type.Text // 消息类型是否为文本
+  // TODO 你们可以根据自己的需求修改这里的逻辑
+  if (isText && content) {
+    let notification = `发送“${botName}【问题】”可开启chatgpt智能会话，例如“${botName} 老婆和我妈同时掉河里，先救谁？”`
+    const callChat = content.includes(`${botName}`) // 艾特了机器人
+    try {
+      // 区分群聊和私聊
+      if (callChat) {
+        if(content == notification || content.includes("[chatgpt response]")){
+          return 
+        }
+
+        let contentString = content.replace(`${botName}`, '').trim()
+        if(room){
+          await room.say(pre + await getReply(contentString))
+          return
+        } else {
+          if(!chatMap.has(name)){
+            chatMap.set(name, new Date().getTime())
+          }
+          await contact.say(pre + await getReply(contentString))
+        }
+      } else {
+        let nowTime = new Date().getTime()
+        for(let name of chatMap.keys()){
+          if(nowTime - chatMap.get(name) > 1000 * 60 * 60 * 24){
+            chatMap.delete(name)
+          }
+        }
+
+        if(!room){
+          if(!chatMap.has(name)){
+            chatMap.set(name, new Date().getTime())
+            await contact.say(notification)
+          }
+        }
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
 }
 
 // 初始化机器人
-export const bot = WechatyBuilder.build({
-  name: 'WechatEveryDay',
-  // puppet: 'wechaty-puppet-wechat4u', // 如果有token，记得更换对应的puppet
-  puppet: 'wechaty-puppet-wechat', // 如果 wechaty-puppet-wechat 存在问题，也可以尝试使用上面的 wechaty-puppet-wechat4u ，记得安装 wechaty-puppet-wechat4u
-  puppetOptions: {
-    uos: true,
-  },
-})
+async function initProject() {
+  bot = WechatyBuilder.build({
+    name: 'WechatEveryDay',
+    puppet: 'wechaty-puppet-wechat', // 如果有token，记得更换对应的puppet
+    puppetOptions: {
+      uos: true,
+      stealthless: true
+    },
+  });
 
-// 扫码
-bot.on('scan', onScan)
-// 登录
-bot.on('login', onLogin)
-// 登出
-bot.on('logout', onLogout)
-// 收到消息
-bot.on('message', onMessage)
-// 添加好友
-bot.on('friendship', onFriendShip)
+  // 扫码
+  bot.on('scan', onScan)
+  // 登录
+  bot.on('login', onLogin)
+  // 登出
+  bot.on('logout', onLogout)
+  // 收到消息
+  bot.on('message', onMessage)
+  // 添加好友
+  bot.on('friendship', onFriendShip)
 
-// 启动微信机器人
-bot
-  .start()
-  .then(() => console.log('Start to log in wechat...'))
-  .catch((e) => console.error(e))
+  // 启动微信机器人
+  bot
+    .start()
+    .then(() => console.log('Start to log in wechat...'))
+    .catch((e) => console.error(e))
+}
