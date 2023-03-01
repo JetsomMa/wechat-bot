884f2917e080b153748c946fa46b40a732bab689
mashaojie <mashaojie@pinefield-inc.com>
2023-03-01 18:02:27 +0800

    perf: 修改代理配置

--- wechat-bot.js
+++ wechat-bot.js
@@ -2,7 +2,7 @@ import { patch } from 'happy-eyeballs';
 import { Agent as HttpAgent } from 'http';
 import { Agent as HttpsAgent } from 'https';
 
-patch(HttpAgent, {delay: 1000});
-patch(HttpsAgent, {delay: 1000});
+patch(HttpAgent, {delay: 10000});
+patch(HttpsAgent, {delay: 10000});
 
 import "./src/wechaty/index.js";
