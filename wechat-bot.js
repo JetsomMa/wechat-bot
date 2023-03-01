5af2b5e9e7d1c68792402ebaad40b34e79bb66fe
mashaojie <mashaojie@pinefield-inc.com>
2023-03-01 18:22:45 +0800

    perf: 完善Action管理器

--- wechat-bot.js
+++ wechat-bot.js
@@ -2,7 +2,7 @@ import { patch } from 'happy-eyeballs';
 import { Agent as HttpAgent } from 'http';
 import { Agent as HttpsAgent } from 'https';
 
-patch(HttpAgent, {delay: 10000});
-patch(HttpsAgent, {delay: 10000});
+patch(HttpAgent, {delay: 30000});
+patch(HttpsAgent, {delay: 30000});
 
 import "./src/wechaty/index.js";
