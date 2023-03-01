e42170930b0c7315b5a64367c6b1e20bcf33c372
mashaojie <mashaojie@pinefield-inc.com>
2023-03-01 17:11:39 +0800

    perf: 修改代理配置

--- src/chatgpt/index.js
+++ src/chatgpt/index.js
@@ -6,7 +6,7 @@ const env = dotenv.config().parsed // 环境参数
 let chatOption = {};
 const chatGPT = new ChatGPTUnofficialProxyAPI({
   accessToken: env.OPENAI_ACCESS_TOKEN,
-  apiReverseProxyUrl: 'https://gpt.pawan.krd/backend-api/conversation'
+  // apiReverseProxyUrl: 'https://gpt.pawan.krd/backend-api/conversation'
 })
 
 export async function getChatGPTReply({content, contactId}) {
